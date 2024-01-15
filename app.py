8459002bbf80eccce46c41ccbdf9660b37fd5c59
soap man <soapmans@icloud.com>
2024-01-16 03:27:25 +0800

    format file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import binascii
 
 app = Flask(__name__)
 
+
 def send_wol_magic_packet(mac_address, broadcast_ip="255.255.255.255", port=9):
     mac_bytes = binascii.unhexlify(mac_address.replace(":", "").replace("-", ""))
     magic_packet = b'\xFF' * 6 + mac_bytes * 16
@@ -11,6 +12,7 @@ def send_wol_magic_packet(mac_address, broadcast_ip="255.255.255.255", port=9):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         s.sendto(magic_packet, (broadcast_ip, port))
+
 
 @app.route('/wol', methods=['POST'])
 def wake_on_lan():
@@ -23,6 +25,7 @@ def wake_on_lan():
 
     send_wol_magic_packet(target_mac_address)
     return jsonify({'message': 'Wake-on-LAN信号已发送'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
